b10010b140ec93e152721e4089cd79f5096f82d7
Naran Babha <nbabha@gmail.com>
2023-01-30 17:50:51 -0500

    removing db connection from post

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const {searchData} = require('./parser.js');
 const app = express();
 const port = 3001;
@@ -8,28 +9,27 @@ const uri = "mongodb+srv://naranbabha:kIIsQuR1FBhqAO8v@menuitems.wdbco70.mongodb
 
 const client = new MongoClient(uri);
 const dbName = "Restaurants";
+const db = client.db(dbName);
+const col = db.collection("restaurants");
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json()); 
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
 })
 
 app.post("/search", async (req, res) => {
-  // searchData(req.body.item);
   var restaurants = [];
   await client.connect();
-  const db = client.db(dbName);
-  const col = db.collection("restaurants");
   const cursor = col.find({'city' : req.body.city});
-  // const cursor = col.find({'city' : {'$regex' : req.body.city, '$options' : 'i'}});
   await cursor.forEach((r) => {
     restaurants.push(r)
   });
+  
   console.log(restaurants);
   res.json(searchData(req.body.item, restaurants));
-  // res.json(restaurants);
 
 });
 
