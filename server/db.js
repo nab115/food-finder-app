c86d3a72b42dec745f5c6d0a741ed46a113c309c
Naran Babha <nab115@pitt.edu>
2023-01-31 16:10:19 -0800

    logging env variables

--- server/db.js
+++ server/db.js
@@ -1,5 +1,6 @@
 const { MongoClient } = require("mongodb");
 
+console.log(process.env)
 const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@menuitems.wdbco70.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 client.connect();
@@ -7,4 +8,4 @@ const dbName = "Restaurants";
 const db = client.db(dbName);
 const col = db.collection("restaurants");
 
-module.exports.col = col;
+module.exports.col = col; 
