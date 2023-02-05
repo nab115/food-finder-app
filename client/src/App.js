0c0acdc285e13884485a3015bd419afbbf89a3d5
Naran Babha <nab115@pitt.edu>
2023-02-05 13:41:38 -0800

    refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, {useState, Fragment} from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "./App.css";
 import "./style.css";
-import SearchContainer from "./components/SearchContainer.js"
+import SearchContainer from "./components/SearchPage.js"
 import WelcomeContainer from "./components/WelcomePage";
 
 function App() {
