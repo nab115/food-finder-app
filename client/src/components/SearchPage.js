8653bd700401d256def8e0abf42a096c4a5ab18b
Naran Babha <nab115@pitt.edu>
2023-02-05 21:19:33 -0800

    removing header, seems too nested
    update location on select

--- client/src/components/SearchPage.js
+++ client/src/components/SearchPage.js
@@ -1,21 +1,31 @@
 import React, {useState, Fragment} from 'react';
 
-import search from '../request.js';
-import Header from './Header.js';
+import { search} from '../request.js';
+import SearchBar from "./SearchBar";
+import Dropdown from "./Dropdown";
 import Results from './Results.js';
 
 function SearchContainer({location}) {
 
-    const [data, update] = useState({item: '', results: [], });
+    const [data, update] = useState({ item: '', results: [], location: location });
 
     const searchItem = async (i) => {
-      const value = await search(i, location);
+      const value = await search(i, data.location);
       update({item: i, results: value});
     };
   
+    const changeLocation = (input) => {
+      update({location: input})
+    }
+
     return (
     <Fragment>
-      <Header searchItem={searchItem}/>
+      <div className="header">
+        <h1>Food Finder App</h1>
+        <h2>Hungry? Let's help you out.</h2>
+        <SearchBar inputHandler={searchItem} placeholder="Find something tasty"/>
+        <Dropdown inputHandler={changeLocation} selected={location} placeholder='update your location'/>
+      </div>
       <Results item={data.item} results={data.results}/>
     </Fragment>
     );
