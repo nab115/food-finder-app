22dde4da7c12d0d02c2d56d93ced3c650ebf38f9
Naran Babha <nab115@pitt.edu>
2023-02-06 17:18:27 -0800

    welcome page styling changes

--- client/src/components/WelcomePage.js
+++ client/src/components/WelcomePage.js
@@ -14,10 +14,10 @@ function WelcomePage({setLocation}) {
       <div className='welcome'>
         <div className='welcome-content'>
             <h1>Food Finder App</h1>
-            <h2>Where are you located?</h2>
+            <h3>Where are you located?</h3>
             <form onSubmit={submitHandler} className='input-container'>
                 <span className="welcome-dropdown">
-                    <Dropdown inputHandler={update} placeholder='select your location'/>
+                    <Dropdown inputHandler={update} placeholder='select your city'/>
                 </span>
                 <button className="welcome-button">Go</button>
             </form>
